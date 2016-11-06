18bcd214d63f09d06d5d0b0a4ee6680b85cc89c1
Tom <tom@redpepper.org.uk>
2016-11-06 17:45:44 +0000

    randomised passwords

--- db/m_seeds.rb
+++ db/m_seeds.rb
@@ -1,10 +1,11 @@
 require 'csv'
+require 'securerandom'
 
 puts "Creating Users"
 
 def create_user(email, username)
-  pwd = '12345678'  # should be a random string later
-  puts "    #{username}"
+  pwd = SecureRandom.urlsafe_base64(15)  # random pw A-Z a-z 0-9 and -_
+  puts "    #{username} : #{pwd}"
   User.create!(username: username, email: email, password: pwd, password_confirmation: pwd, confirmed_at: Time.now, terms_of_service: "1")
 end
 
@@ -12,11 +13,13 @@ members = CSV.read("/Users/Tom/Desktop/consul/db/m.csv")
 
 members.each do |email, firstname, lastname|
   
-  username =  firstname.downcase + lastname.downcase.slice(0)
+  username = firstname.downcase + lastname.downcase.slice(0)
 
   user = create_user(email, username) 
   level = 3
   user.update(verified_at: Time.now, document_number: Faker::Number.number(10) )
+
+  # todo: email the user
 
 end
 
