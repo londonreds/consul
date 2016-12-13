283b94e3d7c49b47942dbbcdb81a1083c9e662af
Alex <alex@recordsonribs.com>
2016-12-13 14:42:39 +0000

    Fix undefined variable

--- app/models/proposal_notifier.rb
+++ app/models/proposal_notifier.rb
@@ -32,7 +32,7 @@ class ProposalNotifier
   end
 
   def email_on_comment?
-    return false if proposal.voters.include? @commenter
+    return false if @proposal.voters.include? @commenter
 
     @author != @commenter && @commenter.email_on_proposal_edit_as_commenter?
   end
